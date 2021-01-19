6034c76415a06ce8d9b7cf28fa66ccf3a09ad9c3
Wonyoung Jang <43818471+Stomper10@users.noreply.github.com>
2021-01-19 18:01:15 +0900

    Update run_preprocessing.py

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -11,8 +11,17 @@ import pandas as pd
 ## Preprocessing ##
 ###################
 # Each file contains pairs (path to image, output vector)
-Traindata = pd.read_csv('./CheXpert-v1.0-small/train.csv')
-Traindata = Traindata[500:]
+Traindata_raw = pd.read_csv('./CheXpert-v1.0-small/train.csv')
+paths = list(Traindata_raw['Path'])
+studies = 1
+test_images = []
+for i in range(len(paths) - 1):
+    if paths[i][26:45] != paths[i+1][26:45]:
+        studies += 1
+    if studies <= 500:
+        test_images.append(paths[i])
+
+Traindata = Traindata_raw[len(test_images):]
 Traindata.to_csv('./CheXpert-v1.0-small/train_mod.csv', index = False)
 print('Train data length:', len(Traindata))
 
@@ -20,6 +29,6 @@ Validdata = pd.read_csv('./CheXpert-v1.0-small/valid.csv')
 Validdata.to_csv('./CheXpert-v1.0-small/valid_mod.csv', index = False)
 print('Valid data length:', len(Validdata))
 
-Testdata = Traindata.head(500) # use first 500 training data as test data (observation ratio is almost same as training set!)
+Testdata = Traindata_raw[:len(test_images)] # use first 500 studies from training set as test set (observation ratio is almost same!)
 Testdata.to_csv('./CheXpert-v1.0-small/test_mod.csv', index = False)
 print('Test data length:', len(Testdata))
