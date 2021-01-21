0b1f3fef199de63f205a5c0c497a60908d7c8a38
Wonyoung Jang <43818471+Stomper10@users.noreply.github.com>
2021-01-21 16:12:08 +0900

    Update run_preprocessing.py

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -28,6 +28,7 @@ Traindata_frt.to_csv('./CheXpert-v1.0-small/train_frt.csv', index = False)
 Traindata_lat.to_csv('./CheXpert-v1.0-small/train_lat.csv', index = False)
 print('Train data length(frontal):', len(Traindata_frt))
 print('Train data length(lateral):', len(Traindata_lat))
+print('Train data length(total):', len(Traindata_frt) + len(Traindata_lat))
 
 Validdata = pd.read_csv('./CheXpert-v1.0-small/valid.csv')
 Validdata_frt = Validdata[Validdata['Path'].str.contains('frontal')]
@@ -36,6 +37,7 @@ Validdata_frt.to_csv('./CheXpert-v1.0-small/valid_frt.csv', index = False)
 Validdata_lat.to_csv('./CheXpert-v1.0-small/valid_lat.csv', index = False)
 print('Valid data length(frontal):', len(Validdata_frt))
 print('Valid data length(lateral):', len(Validdata_lat))
+print('Valid data length(total):', len(Validdata_frt) + len(Validdata_lat))
 
 Testdata = Traindata_raw[:border_idx] # use first 500 studies from training set as test set (observation ratio is almost same!)
 Testdata_frt = Testdata[Testdata['Path'].str.contains('frontal')]
@@ -44,3 +46,4 @@ Testdata_frt.to_csv('./CheXpert-v1.0-small/test_frt.csv', index = False)
 Testdata_lat.to_csv('./CheXpert-v1.0-small/test_lat.csv', index = False)
 print('Test data length(frontal):', len(Testdata_frt))
 print('Test data length(lateral):', len(Testdata_lat))
+print('Test data length(total):', len(Testdata_frt) + len(Testdata_lat))
