f8b162ed7df48fabd6c12f4d8ae4ac11903d6803
Wonyoung Jang <43818471+Stomper10@users.noreply.github.com>
2021-02-09 17:01:07 +0900

    Update paths
    Updated paths for renewal.

--- notes.py
+++ notes.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 train = pd.read_csv('./config/train.csv')
-train['Path'] = '/home/leelab/' + train['Path'].astype(str)
+train['Path'] = '/home/jwy4888/leelab/' + train['Path'].astype(str)
 train
 
 dev = pd.read_csv('./config/dev.csv')
-dev['Path'] = '/home/leelab/' + dev['Path'].astype(str)
+dev['Path'] = '/home/jwy4888/leelab/' + dev['Path'].astype(str)
 dev
 
 train.to_csv('./config/train.csv', index=False)
