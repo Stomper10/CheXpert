499ad48cf1d4fe876aa0c4338e774b036550e9a2
Wonyoung Jang <43818471+Stomper10@users.noreply.github.com>
2021-02-19 19:54:10 +0900

    Update run_preprocessing.py

--- run_preprocessing.py
+++ run_preprocessing.py
@@ -22,8 +22,8 @@ border = path_unique[500]
 border_idx = paths.index(border)
 
 Traindata = Traindata_raw[border_idx:]
-Traindata_frt = Traindata[Traindata['Path'].str.contains('frontal')]
-Traindata_lat = Traindata[Traindata['Path'].str.contains('lateral')]
+Traindata_frt = Traindata[Traindata['Path'].str.contains('frontal')].copy()
+Traindata_lat = Traindata[Traindata['Path'].str.contains('lateral')].copy()
 Traindata_frt.to_csv('./CheXpert-v1.0-small/train_frt.csv', index = False)
 Traindata_lat.to_csv('./CheXpert-v1.0-small/train_lat.csv', index = False)
 print('Train data length(frontal):', len(Traindata_frt))
@@ -31,8 +31,8 @@ print('Train data length(lateral):', len(Traindata_lat))
 print('Train data length(total):', len(Traindata_frt) + len(Traindata_lat))
 
 Validdata = Traindata_raw[:border_idx] # use first 500 studies from training set as valid set (observation ratio is almost same!)
-Validdata_frt = Validdata[Validdata['Path'].str.contains('frontal')]
-Validdata_lat = Validdata[Validdata['Path'].str.contains('lateral')]
+Validdata_frt = Validdata[Validdata['Path'].str.contains('frontal')].copy()
+Validdata_lat = Validdata[Validdata['Path'].str.contains('lateral')].copy()
 Validdata_frt.to_csv('./CheXpert-v1.0-small/valid_frt.csv', index = False)
 Validdata_lat.to_csv('./CheXpert-v1.0-small/valid_lat.csv', index = False)
 print('Valid data length(frontal):', len(Validdata_frt))
@@ -40,7 +40,7 @@ print('Valid data length(lateral):', len(Validdata_lat))
 print('Valid data length(total):', len(Validdata_frt) + len(Validdata_lat))
 
 Testdata = pd.read_csv('./CheXpert-v1.0-small/valid.csv')
-Testdata_frt = Testdata[Testdata['Path'].str.contains('frontal')].copy()
+Testdata_frt = Testdata[Testdata['Path'].str.contains('frontal')].copy() # to avoid SettingWithCopyWarning
 Testdata_lat = Testdata[Testdata['Path'].str.contains('lateral')].copy()
 Testdata_frt.to_csv('./CheXpert-v1.0-small/test_frt.csv', index = False)
 Testdata_lat.to_csv('./CheXpert-v1.0-small/test_lat.csv', index = False)
