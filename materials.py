106300e9447ef42394c460bc55ecef816c475f05
Stomper10 <jwy4888@gmail.com>
2021-03-04 16:11:47 +0900

    Debug material.py

--- materials.py
+++ materials.py
@@ -49,22 +49,6 @@ class CheXpertDataSet(Dataset):
             next(csvReader, None) # skip the header
             for line in csvReader:
                 image_name = line[0]
-<<<<<<< HEAD
-                npline = np.array(line)
-                idx = [7, 10, 11, 13, 15]
-                label = list(npline[idx])
-                for i in range(nnClassCount):
-                    if label[i]:
-                        a = float(label[i])
-                        if a == 1:
-                            label[i] = 1
-                        elif a == -1:
-                            label[i] = 1
-                            #if i == 1 or i == 3 or i == 4:  # Atelectasis, Edema, Pleural Effusion
-                            #    label[i] = 1                    # U-Ones
-                            #elif i == 0 or i == 2:          # Cardiomegaly, Consolidation
-                            #    label[i] = 0                    # U-Zeroes
-=======
                 if nnClassCount == 5:
                     npline = np.array(line)
                     idx = [7, 10, 11, 13, 15]
@@ -109,7 +93,6 @@ class CheXpertDataSet(Dataset):
                                     label[i] = 0                     # All U-Zeroes
                             else:
                                 label[i] = 0
->>>>>>> origin/master
                         else:
                             label[i] = 0
                         
